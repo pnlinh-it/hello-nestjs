3b2c3891d961a3033662e6f8d3bc056ec9495d31
Phạm Linh <pnlinh.it@gmail.com>
2021-08-04 21:47:50 +0700

    Export provider

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { UsersService } from './users/users.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly userService: UsersService,
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello() {
+    return this.userService.index();
   }
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,8 +2,13 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
+/**
+ * To use UserService in other module
+ * that import UserModule: exports: [UsersService]
+ */
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
